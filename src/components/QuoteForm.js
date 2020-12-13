c16418450da21efd0dd68f2db958b80af075adc2
HuffDaddy0 <ty.huff24@gmail.com>
2020-12-13 17:17:24 -0500

    Done.

--- src/components/QuoteForm.js
+++ src/components/QuoteForm.js
@@ -6,18 +6,27 @@ import { addQuote } from '../actions/quotes';
 class QuoteForm extends Component {
 
   state = {
-    //set up a controlled form with internal state
+    
   }
 
   handleOnChange = event => {
-    // Handle Updating Component State
+    if (e.target.name === "content"){
+    this.setState({...this.state, content: event.target.value})
+    } else {
+      this.setState({ ...this.state, author: e.target.value})
+    }
   }
 
   handleOnSubmit = event => {
-    // Handle Form Submit event default
-    // Create quote object from state
-    // Pass quote object to action creator
-    // Update component state to return to default state
+    event.preventDefault()
+    let quoteObj = {
+      content: this.state.content,
+      author: this.state.author,
+      votes: 0,
+      id: uuid() 
+    }
+    this.props.addQuote(quoteObj)
+    this.setState({author:"", content:""})
   }
 
   render() {
@@ -32,6 +41,7 @@ class QuoteForm extends Component {
                     <label htmlFor="content" className="col-md-4 control-label">Quote</label>
                     <div className="col-md-5">
                       <textarea
+                        name="content"
                         className="form-control"
                         value={this.state.content}
                       />
@@ -41,6 +51,7 @@ class QuoteForm extends Component {
                     <label htmlFor="author" className="col-md-4 control-label">Author</label>
                     <div className="col-md-5">
                       <input
+                        name="author"
                         className="form-control"
                         type="text"
                         value={this.state.author}
@@ -63,4 +74,4 @@ class QuoteForm extends Component {
 }
 
 //add arguments to connect as needed
-export default connect()(QuoteForm);
+export default connect(null, { addQuote })(QuoteForm);
